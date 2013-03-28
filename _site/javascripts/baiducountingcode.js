8252b2d5d122d81f9f7d32566b284d5a7ab2575f
Daemon <cgyqqcgy@126.com>
2013-03-28 17:24:03 +0800

    change the style

---
+++ _site/javascripts/baiducountingcode.js
@@ -0,0 +1,2 @@
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fd6f13bba8ef861c93267a31c0de34df9' type='text/javascript'%3E%3C/script%3E"));
